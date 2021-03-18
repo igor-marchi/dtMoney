4a358602b43adccbdbe4cc1f23cc9cc6fb8aac99
igor-marchi <igoraugustomarchi@gmail.com>
2021-03-17 21:54:35 -0300

    adicionado configuraçoes de fonte

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --red: ##E62E4D;
         --blue: #5429CC;
         --green: #33CC95;
-        
+
         --blue-light: #6933FF
         
         --text-title:#363F5F;
@@ -34,6 +34,15 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background: var(---background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button {
