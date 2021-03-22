6f963324831411d4213ecd180113fdf2a24e25ce
igor-marchi <igoraugustomarchi@gmail.com>
2021-03-21 23:11:41 -0300

    Adicionado modal e api fake com miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Dashboard } from "./compenents/Dashboard";
 import { Header } from "./compenents/Header";
+import Modal from 'react-modal';
+
 import { GlobalStyle } from "./styles/Global";
+import { NewTransactionModal } from "./compenents/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function HandleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  function HandleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Fragment>
-      <Header />
+      <Header onOpenNewTransactionModal={HandleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={HandleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </Fragment>
   );
